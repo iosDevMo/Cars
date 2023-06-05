f2409913dd16c66e651d819c3a609ed77892632b
mohamdan <engineerm86@gmail.com>
2023-06-05 19:16:30 +0300

    add image assets

---
+++ Cars/Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by mohamdan on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
